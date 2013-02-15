6e877e9d3343e1f5d0294879e3a715d9ce7f2f9d
YASUAKI TAKEDA <sadamitsu82@gmail.com>
2013-02-15 21:01:24 +0900

    test5

---
+++ client.js
@@ -0,0 +1,28 @@
+var io = require('socket.io-client');
+var url = "localhost";
+var options = {
+    'force new connection':true,
+    port:3000
+};
+
+//最大接続ユーザ数
+var maxConnect = 5;
+//接続しているユーザ数
+var connectCounter = 0;
+ 
+var socket;
+for(var i = 0 ;i < maxConnect; i++) {
+    socket = io.connect(url, options);
+    socket.on('connect', function (data) {
+        connectCounter += 1;
+        console.log("connect. connectCounter=" + connectCounter);
+    });
+ 
+    socket.on('sendMsgFromServer', function (msg) {
+        console.log("message:",msg);
+    });
+}
+ 
+setTimeout(function(){
+    socket.emit("sendMsgFromClient","send client msg");
+},3000);
