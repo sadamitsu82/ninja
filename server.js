1d01618904a69bb49d32611948d24a9f352b1ed6
YASUAKI TAKEDA <sadamitsu82@gmail.com>
2013-02-15 20:17:51 +0900

    test3

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  console.log('request url: ', req.url);
-}).listen(80);
+var net = require('net');
+var server = net.createServer(function(socket) {
+    socket.setEncoding('utf8');
+    
+    socket.on('connect', function() {
+       console.log('‚Ù‚Þ‚Ù‚Þ');
+    });
+});
+   
+server.listen(3000);
+console.log('Server runninng at:3000');
