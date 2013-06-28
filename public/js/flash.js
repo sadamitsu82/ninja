707c1446eb34c8a232b0b1058b1144bd1b66f784
YASUAKI TAKEDA <sadamitsu82@gmail.com>
2013-06-28 20:01:53 +0900

    test26

--- public/js/flash.js
+++ public/js/flash.js
@@ -3,10 +3,5 @@ window.onload=function(){
 		var params = {allowscriptaccess: "always"};
 		var attributes = {};
 		swfobject.embedSWF("/swf/index.swf", "contents", "200", "200", "10", "", flashvars, params, attributes);
-};
-window.onload=function(){
-		var flashvars = {};
-		var params = {allowscriptaccess: "always"};
-		var attributes = {};
 		swfobject.embedSWF("/swf/index2.swf", "contents2", "200", "200", "10", "", flashvars, params, attributes);
 };
